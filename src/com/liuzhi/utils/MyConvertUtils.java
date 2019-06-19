57b1814de44036e8b2b9b0143aafb061bea36d6c
LiuZhi <bonzer@vip.qq.com>
2019-06-19 18:01:54 +0800

    对象转换类修复

--- src/com/liuzhi/utils/MyConvertUtils.java
+++ src/com/liuzhi/utils/MyConvertUtils.java
@@ -1,17 +1,17 @@
 package com.liuzhi.utils;
 
+import java.io.File;
 import java.lang.reflect.Field;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.*;
 
 /**
  * 对象转换类
  */
 public class MyConvertUtils
 {
-    public static  <T>T MapToObject(Map map,Class<T> tClass)
+    public static  <T>T mapToObject(Map map,Class<T> tClass)
     {
-        T t = null;
+        T t;
         try {
             t = tClass.newInstance();
         } catch (InstantiationException e) {
@@ -21,16 +21,28 @@ public class MyConvertUtils
             e.printStackTrace();
             return null;
         }
-        Field[] fields = tClass.getFields();
+        List<Field> list = new ArrayList<>();
+
+        for(Field f : tClass.getFields())
+        {
+            list.add(f);
+        }
+        for(Field f : tClass.getDeclaredFields())
+        {
+            list.add(f);
+        }
+
         Iterator<Map.Entry<String,Object>> iterator = map.entrySet().iterator();
         while (iterator.hasNext())
         {
             Map.Entry<String,Object> entry = iterator.next();
-            for(Field field : fields)
+            for(Field field : list)
             {
                 if(field.getName().equalsIgnoreCase(entry.getKey()))
                 {
                     try {
+                        //不完善的,这里只能转普通的String类,如果是其他类型,必然报错!
+                        field.setAccessible(true);
                         field.set(t,entry.getValue());
                     } catch (IllegalAccessException e) {
                         e.printStackTrace();
