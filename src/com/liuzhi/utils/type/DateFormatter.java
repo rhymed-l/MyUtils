7f0a57502567c70fd5d43c1fccddefdfd8946a6f
LiuZhi <bonzer@vip.qq.com>
2019-06-27 17:36:28 +0800

    12小时制

--- src/com/liuzhi/utils/type/DateFormatter.java
+++ src/com/liuzhi/utils/type/DateFormatter.java
@@ -2,25 +2,25 @@ package com.liuzhi.utils.type;
 
 /**
  * 时间格式化枚举
- *     TIME_FORMAT_YMD_G_HMS  ("yyyy-MM-dd hh:mm:ss"    )  ,
+ *     TIME_FORMAT_YMD_G_HMS  ("yyyy-MM-dd HH:mm:ss"    )  ,
  *     TIME_FORMAT_YMD_G      ("yyyy-MM-dd"             )  ,
  *     TIME_FORMAT_YMD_H      ("yyyy/MM/dd"             )  ,
- *     TIME_FORMAT_YMD_H_HMS  ("yyyy/MM/dd hh:mm:ss"    )  ,
- *     TIME_FORMAT_TIMESTMP   ("yyyyMMddhhmmss"         )  ,
+ *     TIME_FORMAT_YMD_H_HMS  ("yyyy/MM/dd HH:mm:ss"    )  ,
+ *     TIME_FORMAT_TIMESTMP   ("yyyyMMddHHmmss"         )  ,
  *     TIME_FORMAT_TIME       ("yyyyMMdd"               )  ,
  *     TIME_FORMAT_CHINA      ("yyyy年MM月dd日"            ),
- *     TIME_FORMAT_CHINASTMP  ("yyyy年MM月dd日 hh:mm:ss"   );
+ *     TIME_FORMAT_CHINASTMP  ("yyyy年MM月dd日 HH:mm:ss"   );
  */
 public enum DateFormatter
 {
-    TIME_FORMAT_YMD_G_HMS  ("yyyy-MM-dd hh:mm:ss"    )  ,
+    TIME_FORMAT_YMD_G_HMS  ("yyyy-MM-dd HH:mm:ss"    )  ,
     TIME_FORMAT_YMD_G      ("yyyy-MM-dd"             )  ,
     TIME_FORMAT_YMD_H      ("yyyy/MM/dd"             )  ,
-    TIME_FORMAT_YMD_H_HMS  ("yyyy/MM/dd hh:mm:ss"    )  ,
-    TIME_FORMAT_TIMESTMP   ("yyyyMMddhhmmss"         )  ,
+    TIME_FORMAT_YMD_H_HMS  ("yyyy/MM/dd HH:mm:ss"    )  ,
+    TIME_FORMAT_TIMESTMP   ("yyyyMMddHHmmss"         )  ,
     TIME_FORMAT_TIME       ("yyyyMMdd"               )  ,
     TIME_FORMAT_CHINA      ("yyyy年MM月dd日"            ),
-    TIME_FORMAT_CHINASTMP  ("yyyy年MM月dd日 hh:mm:ss"   );
+    TIME_FORMAT_CHINASTMP  ("yyyy年MM月dd日 HH:mm:ss"   );
 
     private final String formatter;
 
