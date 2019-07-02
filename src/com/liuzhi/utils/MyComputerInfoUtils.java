05350e2b451abd845b66930b0c7ec7b1cc1488a4
LiuZhi <bonzer@vip.qq.com>
2019-07-02 16:40:04 +0800

    电脑信息

---
+++ src/com/liuzhi/utils/MyComputerInfoUtils.java
@@ -0,0 +1,50 @@
+package com.liuzhi.utils;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Properties;
+
+public class MyComputerInfoUtils
+{
+    private static Properties props = System.getProperties();
+    private static InetAddress addr;
+
+    static {
+        try {
+            addr = InetAddress.getLocalHost();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * 获取操作系统名
+     */
+    public static String getSystemName()
+    {
+        return props.getProperty("os.name");
+    }
+
+    /**
+     * 获取操作系统版本
+     */
+    public static String getgetSystemVersion()
+    {
+        return props.getProperty("os.version");
+    }
+    /**
+     * 获取电脑名称
+     */
+    public static String getComputerName()
+    {
+        return addr.getHostName();
+    }
+    /**
+     * 获取电脑IP地址
+     */
+    public static String getComputerIpAddress()
+    {
+        return addr.getHostAddress();
+    }
+}
