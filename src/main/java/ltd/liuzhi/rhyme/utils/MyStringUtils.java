adf36665ff1b8977334fec72693a0a6bf0b2396c
bonzer <bonzer@qq.com>
2020-04-27 23:02:23 +0800

    字符串类增加出现次数

--- src/main/java/ltd/liuzhi/rhyme/utils/MyStringUtils.java
+++ src/main/java/ltd/liuzhi/rhyme/utils/MyStringUtils.java
@@ -5,6 +5,7 @@ import java.io.LineNumberReader;
 import java.io.StringReader;
 import java.security.MessageDigest;
 import java.util.*;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MyStringUtils
@@ -733,5 +734,39 @@ public class MyStringUtils
             str =  str.replaceAll("  "," ");
         }
         return str;
+    }
+
+    /**
+     * 获取指定字符串出现的次数(正则表达式)
+     *
+     * @param srcText 源字符串
+     * @param findText 要查找的字符串
+     * @return 出现的次数
+     */
+    public static int appearNumberRe(String srcText, String findText) {
+        int count = 0;
+        Pattern p = Pattern.compile(findText);
+        Matcher m = p.matcher(srcText);
+        while (m.find()) {
+            count++;
+        }
+        return count;
+    }
+
+    /**
+     * 获取指定字符串出现的次数
+     *
+     * @param srcText 源字符串
+     * @param findText 要查找的字符串
+     * @return 出现的次数
+     */
+    public static int appearNumber(String srcText, String findText) {
+        int count = 0;
+        int index = 0;
+        while ((index = srcText.indexOf(findText, index)) != -1) {
+            index = index + findText.length();
+            count++;
+        }
+        return count;
     }
 }
